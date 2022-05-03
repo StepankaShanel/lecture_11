2fdde3ee6f674d9d3acb42429957a0536e482c3b
StepankaShanel <stepashanelova@seznam.cz>
2022-05-03 17:14:12 +0200

    hotovo

--- fibo.py
+++ fibo.py
@@ -1,6 +1,15 @@
+def recursive_nth_fibo(number):
+    if number == 0:
+        return number
+    elif number == 1:
+        return 1
+    else:
+        return (recursive_nth_fibo(number - 1) + recursive_nth_fibo(number - 2))
+
+
 
 def main():
-    pass
+    print(recursive_nth_fibo(10))
 
 
 if __name__ == "__main__":
